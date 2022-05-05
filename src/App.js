5697dd5453e24460fc67838c3e2f55a7080fb90f
Khayman King <khaymanak@gmail.com>
2022-05-05 16:20:11 -0700

    setup my bear response in use effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { getBears } from './services/fetch-utils';
+import BearList from './BearList';
 
 function App() {
+  const [bears, setBears] = useState([]);
+
+  useEffect(async () => {
+    const bearResponse = await getBears();
+
+    setBears(bearResponse);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/BearList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BearList() {
+  return (
+    <div>BearList</div>
+  );
+}
