2c41ee55e85d091e1ff68f1445a5040516b75f91
Khayman King <khaymanak@gmail.com>
2022-05-06 09:52:16 -0700

    added cartoon list to ap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
-import { getBears, getCandies, getSpiders } from './services/fetch-utils';
+import { getBears, getCandies, getSpiders, getCartoons } from './services/fetch-utils';
 import BearList from './BearList';
 import CandiesList from './CandiesList';
 import SpiderList from './SpiderList';
+import CartoonList from './CartoonList';
 import './App.css'; 
+
 function App() {
   const [bears, setBears] = useState([]);
   const [candies, setCandies] = useState([]);
   const [spiders, setSpiders] = useState([]);
+  const [cartoons, setCarttons] = useState([]);
 
   useEffect(async () => {
     const bearResponse = await getBears();
     const candyResponse = await getCandies();
     const spiderResponse = await getSpiders();
-
+    const cartoonRespone = await getCartoons();
 
     setBears(bearResponse);
     setCandies(candyResponse);
@@ -33,6 +36,10 @@ function App() {
       <div>
         <h1>Spiders</h1>
         <SpiderList spiders={spiders}/>
+      </div>
+      <div>
+        <h1>Cartoons</h1>
+        <CartoonList cartoons={cartoons}/>
       </div>
     </div>
   );
