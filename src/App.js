6655058906087579c5ee362be0a26e1564b3f7c4
Khayman King <khaymanak@gmail.com>
2022-05-05 17:11:42 -0700

    now rendering bears in app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
 import { getBears } from './services/fetch-utils';
 import BearList from './BearList';
-
+import './App.css'; 
 function App() {
-  const [bears, setBears] = useState([]);
+  const [bears, setBears] = useState([{
+    type: 'brown',
+    color: 'brown',
+    location: 'North America'
+  }]);
 
   useEffect(async () => {
     const bearResponse = await getBears();
 
     setBears(bearResponse);
-  });
-
+  }, []);
   return (
     <div className="App">
-
+      <BearList bears={bears}/>
     </div>
   );
 }
--- src/BearList.js
+++ src/BearList.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export default function BearList() {
+export default function BearList({ bears }) {
   return (
-    <div>BearList</div>
+    <div className='bear-list'>
+      {
+        bears.map(bear => 
+          <div className='bear' key={bear.type}>
+            <p>Type of Bear: {bear.type}</p>
+            <p>Bear Color: {bear.color}</p>
+            <p> Located: {bear.location}</p>
+          </div>
+        )
+      }
+    </div>
   );
 }
